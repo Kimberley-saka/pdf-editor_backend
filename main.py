c9c02f5e6b03aa7c652c45decb86f0183befd53e
Kimberley-saka <kimberleysaka@gmail.com>
2024-02-28 10:31:19 +0200

    added a dependency function

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 fastAPI app
 """
 from fastapi import FastAPI
+from db.config import SessionLocal, engine
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
