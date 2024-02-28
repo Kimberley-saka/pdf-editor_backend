41c4eca01be7e7e0e8c901cdb59f87eecfc2739e
Kimberley-saka <kimberleysaka@gmail.com>
2024-02-28 10:21:29 +0200

    updated

--- db/models/models.py
+++ db/models/models.py
@@ -3,9 +3,9 @@ User database model
 """
 
 
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from .pdf import PDF
+
 from db.config import Base
 
 class User(Base):
@@ -22,4 +22,18 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
 
-    pdfs = relationship(PDF, back_populates="user")
+    pdfs = relationship("PDF", back_populates="user")
+
+
+class PDF(Base):
+    """
+    pdf model
+    """
+    __tablename__ = "pdfs"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    file = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User", back_populates="pdfs")
